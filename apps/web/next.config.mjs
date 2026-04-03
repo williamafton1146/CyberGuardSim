0054ae5339be94d42a553b602e5ed46536e3a5dd
williamafton <kseniaskarbanenko@gmail.com>
2026-04-03 22:03:52 +0300

    Delete apps directory

--- apps/web/next.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
-    externalDir: true
-  },
-  transpilePackages: ["@cyber-sim/shared"]
-};
-
-export default nextConfig;
